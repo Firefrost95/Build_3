5d84b56a24160f96ebe0e2fef6ee6ff94b16fa66
Firefrost95 <119331163+Firefrost95@users.noreply.github.com>
2023-11-10 09:47:09 +0100

    Delete src/config.h

--- src/config.h
+++
@@ -1,9 +0,0 @@
-const char* ssid = "Proximus-Home-850113";
-const char* password = "44a4dxh37e3wpfab";
-const char* mqttServer = "192.168.129.18";
-const int mqttPort = 1883;
-const char* mqttUser = "";
-const char* mqttPassword = "";
-const char* mqttTopic = "sensor1/temp";
-const char* mqttTopic1 = "esp32/input";
-
